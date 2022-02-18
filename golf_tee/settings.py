07acb163563651546d2741e5f0b601612d148555
rainerioagbayani <agbayanieri21@gmail.com>
2022-02-18 15:09:07 +0800

    -test

--- golf_tee/settings.py
+++ golf_tee/settings.py
@@ -217,7 +217,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
