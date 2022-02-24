026f1d33d5c35a36dfc0f81dc4079a173be0f230
rainerioagbayani <agbayanieri21@gmail.com>
2022-02-24 09:03:40 +0800

    debug false. fixed css.

--- golf_tee/settings.py
+++ golf_tee/settings.py
@@ -36,7 +36,7 @@ PAYPAL_CLIENT_SECRET = os.environ["PAYPAL_CLIENT_SECRET"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
